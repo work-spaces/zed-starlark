7c0ea2ba8343e16d23fbcbb9fa843d1106cb8f88
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2026-02-08 18:47:50 -0700

    Trim down to just spaces run-lsp

--- src/starlark.rs
+++ src/starlark.rs
@@ -1,15 +1,10 @@
-use starpls::Starpls;
 use zed_extension_api::{self as zed, settings::LspSettings, Result};
 
-mod starpls;
-
-struct StarlarkExtension {
-    starpls: Option<Starpls>,
-}
+struct StarlarkExtension {}
 
 impl zed::Extension for StarlarkExtension {
     fn new() -> Self {
-        Self { starpls: None }
+        Self {}
     }
 
     fn language_server_command(
@@ -18,31 +13,6 @@ impl zed::Extension for StarlarkExtension {
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         match language_server_id.as_ref() {
-            "starpls" => {
-                let binary_settings = LspSettings::for_worktree("starpls", worktree)
-                    .ok()
-                    .and_then(|lsp_settings| lsp_settings.binary);
-                let binary_args = binary_settings
-                    .as_ref()
-                    .and_then(|binary_settings| binary_settings.arguments.clone())
-                    .unwrap_or_else(|| vec![]);
-                let starpls = self.starpls.get_or_insert_with(|| Starpls::new());
-                Ok(zed::Command {
-                    command: starpls.language_server_binary_path(language_server_id)?,
-                    args: binary_args,
-                    env: Default::default(),
-                })
-            }
-            "buck2-lsp" => {
-                let path = worktree.which("buck2").ok_or_else(|| {
-                    "buck2 must be installed. The LSP is bundled with the buck2 cli.".to_string()
-                })?;
-                Ok(zed::Command {
-                    command: path,
-                    args: vec!["lsp".to_string()],
-                    env: Default::default(),
-                })
-            }
             "spaces-lsp" => {
                 let path = worktree.which("spaces").ok_or_else(|| {
                     "spaces must be installed. The LSP is part of the spaces binary".to_string()
@@ -50,17 +20,6 @@ impl zed::Extension for StarlarkExtension {
                 Ok(zed::Command {
                     command: path,
                     args: vec!["run-lsp".to_string()],
-                    env: Default::default(),
-                })
-            }
-            "tilt" => {
-                let path = worktree.which("tilt").ok_or_else(|| {
-                    "`tilt` must be installed. The LSP is bundled with the tilt cli.".to_string()
-                })?;
-
-                Ok(zed::Command {
-                    command: path,
-                    args: vec!["lsp".to_string(), "start".to_string()],
                     env: Default::default(),
                 })
             }
